8ff239480f453d150fc5b85df2ec27c0c7b1a490
Andres <andres039039@Hotmail.com>
2021-11-04 22:34:45 +0000

    files are linted

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { fetchMyIp } = require('./iss') 
+const { fetchMyIp } = require("./iss");
 
-fetchMyIp((error, ip) => {
-  if (error) {
-    console.log('it did not work dude!', error);
-    return;
-  }
-  console.log('It worked 🐯, the returned IP:', ip['ip']);
-})
+// fetchMyIp((error, ip) => {
+//   if (error) {
+//     console.log('it did not work dude!', error);
+//     return;
+//   }
+//   console.log('It worked 🐯, the returned IP:', ip['ip']);
+// })
--- iss.js
+++ iss.js
@@ -1,12 +1,20 @@
-'use strict';
-const request = require('request')
+"use strict";
+const request = require("request");
 const fetchMyIp = function(callback) {
-request('https://api.ipify.org?format=json', function (error, response, body) {
-  if(error) {
-    callback(error, null)
+  request(
+    "https://api.ipify.org?format=json",
+    function(error, response, body) {
+      if (error) {
+        callback(error, null);
+      }
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      callback(null, JSON.parse(body));
     }
-    callback(null, JSON.parse(body))
-  }
-)}
+  );
+};
 
-module.exports = {fetchMyIp}
+module.exports = { fetchMyIp };
